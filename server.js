b8f2d35aff807524fc062ff3dbce7b0925caf02d
Adrien Bardet <nawaac.dofus@gmail.com>
2025-11-26 17:32:31 +0100

    Add server.js for local development

--- server.js
+++ server.js
@@ -2,28 +2,37 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const PORT = 8080;
+
+const MIME_TYPES = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.ico': 'image/x-icon',
+};
+
 const server = http.createServer((req, res) => {
     let filePath = '.' + req.url;
-    if (filePath == './') filePath = './index.html';
+    if (filePath === './') {
+        filePath = './index.html';
+    }
 
     const extname = path.extname(filePath);
-    let contentType = 'text/html';
-    switch (extname) {
-        case '.js': contentType = 'text/javascript'; break;
-        case '.css': contentType = 'text/css'; break;
-        case '.json': contentType = 'application/json'; break;
-        case '.png': contentType = 'image/png'; break;
-        case '.jpg': contentType = 'image/jpg'; break;
-    }
+    const contentType = MIME_TYPES[extname] || 'application/octet-stream';
 
     fs.readFile(filePath, (error, content) => {
         if (error) {
-            if (error.code == 'ENOENT') {
-                res.writeHead(404);
-                res.end('404 Not Found');
+            if (error.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('404 Not Found', 'utf-8');
             } else {
                 res.writeHead(500);
-                res.end('500: ' + error.code);
+                res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
             }
         } else {
             res.writeHead(200, { 'Content-Type': contentType });
@@ -32,5 +41,6 @@ const server = http.createServer((req, res) => {
     });
 });
 
-server.listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
